b5ef583df7203a2825491faa5c14e2f52f6e23f3
xiaozai <ysongonly@gmail.com>
2022-02-01 16:39:00 +0200

    fix

--- pytracking/tracker/depth_segm/depth_segm.py
+++ pytracking/tracker/depth_segm/depth_segm.py
@@ -342,10 +342,10 @@ class DepthSegm(BaseTracker):
         mean_target_sz=[mean_h, mean_w]
         change_ratio = abs(new_ratio-mean_historyratios)/mean_historyratios
 
-        ratio_flag = False
+        target_sz_ratio_flag = False
         if change_ratio>0.50:
             self.target_sz=torch.FloatTensor(mean_target_sz)
-            ration_flag = True
+            target_sz_ratio_flag = True
 
         if self.frame_num<self.params.frames_true_validd:
             self.valid_d=True
@@ -382,7 +382,7 @@ class DepthSegm(BaseTracker):
                 self.history_info['pos']=self.history_info['pos'][-self.params.num_history:]
 
         # target lost
-        if (self.score_map.max()<self.params.threshold_force_redetection) or (self.flag=='not_found' and self.valid_d==False) or (ration_flag):
+        if (self.score_map.max()<self.params.threshold_force_redetection) or (self.flag=='not_found' and self.valid_d==False) or (target_sz_ratio_flag):
 
             self.redetection_mode=True
             # self.tracker_fail=True
