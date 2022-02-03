edfd848817e5cd98dc1c9938e622182d59aef713
xiaozai <ysongonly@gmail.com>
2022-02-03 17:02:42 +0200

    fix redetection scale

--- pytracking/tracker/depth_segm/depth_segm.py
+++ pytracking/tracker/depth_segm/depth_segm.py
@@ -516,7 +516,7 @@ class DepthSegm(BaseTracker):
             print('....... In redetection Mode.....')
             self.target_scale_redetection=torch.tensor(self.target_scale_redetection*1.05) #slowing enlarge this area to the object
             # self.target_scale_redetection=max(self.target_scale_redetection, self.min_scale_factor)
-            self.target_scale_redetection=min(self.target_scale_redetection, 5*self.first_target_scale) # 2*self.first_target_scale
+            self.target_scale_redetection=min(self.target_scale_redetection, 3*self.first_target_scale) # 2*self.first_target_scale
             print('self.target_scale_redetection : ', self.target_scale_redetection)
             scores_re, pred_segm_region = self.one_pass_track(color, depth, self.target_scale_redetection)
 
