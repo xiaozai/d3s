d5fe991358d48e34199e33bfd6cbff4a44ec3cd9
xiaozai <ysongonly@gmail.com>
2022-02-03 19:25:01 +0200

    fix

--- pytracking/tracker/depth_segm/depth_segm.py
+++ pytracking/tracker/depth_segm/depth_segm.py
@@ -524,16 +524,16 @@ class DepthSegm(BaseTracker):
             scores_re, pred_segm_region = self.one_pass_track(color, depth, self.target_scale_redetection)
 
             print('....Redetection results ', scores_re.max()[0].item(), self.params.target_refound_threshold, self.valid_d)
-
             self.update_dal_longterm_params(depth)
+            print('....Redetection results ', scores_re.max()[0].item(), self.params.target_refound_threshold, self.valid_d)
 
-            # self.redetection_mode=True
+            self.redetection_mode=True
             # DAL longter settings, target re-detected conditions
-            # if scores_re.max()[0].item()>=self.params.target_refound_threshold and self.valid_d:
-            #     print('....Redetection mode finnish.....', scores_re.max()[0].item(), self.params.target_refound_threshold, self.valid_d)
-            #     self.redetection_mode=False
-            # if scores_re.max()[0].item()>=self.params.target_forcerefound_threshold:
-            #     self.redetection_mode=False
+            if scores_re.max()[0].item()>=self.params.target_refound_threshold and self.valid_d:
+                print('....Redetection mode finnish.....', scores_re.max()[0].item(), self.params.target_refound_threshold, self.valid_d)
+                self.redetection_mode=False
+            if scores_re.max()[0].item()>=self.params.target_forcerefound_threshold:
+                self.redetection_mode=False
 
 
             ''' target refound '''
