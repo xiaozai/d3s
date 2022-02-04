e75b3434a2017c10c2b141429d2f0e06da85d943
xiaozai <ysongonly@gmail.com>
2022-02-04 12:43:43 +0200

    fix max min depth

--- pytracking/tracker/base/basetracker.py
+++ pytracking/tracker/base/basetracker.py
@@ -374,7 +374,7 @@ class BaseTracker:
             # depth = cv.normalize(depth, None, alpha=0, beta=1, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
             print('... max depth : min depth : ', max_depth, min_depth)
             depth = (depth - min_depth) / (max_depth - min_depth) * 1.0
-            depth = np.clip(depth, 0, 1.2)
+            depth = np.clip(depth, -0.2, 1.2)
             depth = np.expand_dims(np.asarray(depth), axis=-1)
             images = {'color':color, 'depth':depth}
             return images
