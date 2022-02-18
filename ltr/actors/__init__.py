0ea8c76a62066bc1f80bf667ceefd247c58b386a
xiaozai <ysongonly@gmail.com>
2022-02-18 14:46:46 +0200

    fix

--- ltr/actors/__init__.py
+++ ltr/actors/__init__.py
@@ -1,3 +1,3 @@
 from .base_actor import BaseActor
 from .segm_actor import SegmActor
-from .depth_segm_actor import DepthSegmActor, DepthSegmAttentionActor
+from .depth_segm_actor import DepthSegmActor
