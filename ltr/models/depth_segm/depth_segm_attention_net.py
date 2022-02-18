6788ebda9b95d8b2eb70a69216590d7f35ed96e3
xiaozai <ysongonly@gmail.com>
2022-02-18 17:45:01 +0200

    fix

--- ltr/models/depth_segm/depth_segm_attention_net.py
+++ ltr/models/depth_segm/depth_segm_attention_net.py
@@ -400,15 +400,17 @@ class DepthSegmNetAttention(nn.Module):
                                torch.cat((self.f_layers[layer](f_train_rgb*bg_mask), self.d_layers[layer](f_train_d*bg_mask)), dim=3)),
                                dim=2)
         # feat_rgbd2, featuremaps for each pacth
-        feat_rgbd, attn_weights = self.rgbd_transformers[layer](feat_rgbd) # [B, Patches, C=768], attn_weights, [B, heads=12, patches, headsize=64]
+        feat_rgbd, attn_weights = self.rgbd_transformers[layer](feat_rgbd)        # [B, Patches, C=768], attn_weights, [B, heads=12, patches, headsize=64]
         n_patches, featmap_sz = feat_rgbd.shape[1] // 4, feat_rgbd.shape[1] // 16 # for each patch, 4x4, 16x16, 64x64
         # Only keep test F_rgb and F_D, [B, Patches//2=32/128x512, C=768]
-        feat_rgbd = torch.cat((feat_rgbd[:, :n_patches, :], feat_rgbd[:, n_patches:2*n_patches, :]), dim=-1)
-        feat_rgbd = feat_rgbd.view(feat_rgbd.shape[0], featmap_sz, featmap_sz, -1).permute(0, 3, 1, 2).contiguous() # [B, 4, 4, 2C], [B, 16, 16, 2C], [B, 64, 64, 2C]
-        feat_rgbd = F.interpolate(feat_rgbd, size=(f_test_rgb.shape[-2], f_test_rgb.shape[-1]))                     # B x 2C x 4 x 4 ->  B x 2C x 48 x 48
-        print(layer)
+        feat_rgbd = torch.cat((feat_rgbd[:, :n_patches, :], feat_rgbd[:, n_patches:2*n_patches, :]), dim=-1) # B x 16 x 2C, B x 256 x 2C, B x 64 *x
         print(feat_rgbd.shape)
-        print(pre_out.shape)
+        feat_rgbd = feat_rgbd.view(feat_rgbd.shape[0], featmap_sz, featmap_sz, -1).permute(0, 3, 1, 2).contiguous() # [B, 4, 4, 2C], [B, 16, 16, 2C], [B, 64, 64, 2C]
+        print(feat_rgbd.shape)
+        feat_rgbd = F.interpolate(feat_rgbd, size=(f_test_rgb.shape[-2], f_test_rgb.shape[-1]))                     # B x 2C x 4 x 4 ->  B x 2C x 48 x 48
+        print(layer) = 1
+        print(feat_rgbd.shape) # B x 384 x 96 x 96
+        print(pre_out.shape)   # B x 16 x 96 x 96
         print(self.a_layers[layer])
         print(self.s_layers[layer])
         out = self.post_layers[layer](F.upsample(self.a_layers[layer](feat_rgbd) + self.s_layers[layer](pre_out), scale_factor=2))
