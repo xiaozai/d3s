c7646c5bd79c85cb626f1bb1756cef4d21558bce
xiaozai <ysongonly@gmail.com>
2022-02-23 16:08:11 +0200

    fix

--- ltr/actors/depth_segm_actor.py
+++ ltr/actors/depth_segm_actor.py
@@ -15,16 +15,15 @@ def draw_axis(ax, img, title, show_minmax=False):
     ax.set_title(title, fontsize=9)
 
 def process_attn_maps(att_mat):
-    att_mat = torch.stack(att_mat).squeeze(1)
-    print(att_mat.shape)
+    # use batch 0
+    att_mat = torch.stack(att_mat)
+    att_mat = att_mat[:, 0, ...].squeeze(1) # [layers=3, B, heads=3, 144, 144]
     # Average the attention weights across all heads.
-    att_mat = torch.mean(att_mat, dim=1)
-    print(att_mat.shape)
+    att_mat = torch.mean(att_mat, dim=1) # [layers, B, 144, 144]
     # To account for residual connections, we add an identity matrix to the
     # attention matrix and re-normalize the weights.
     residual_att = torch.eye(att_mat.size(1))
-    print(residual_att.shape)
-    aug_att_mat = att_mat + residual_att
+    aug_att_mat = att_mat + residual_att # [layers, 144, 144]
     aug_att_mat = aug_att_mat / aug_att_mat.sum(dim=-1).unsqueeze(-1)
 
     # Recursively multiply the weight matrices
@@ -38,9 +37,7 @@ def process_attn_maps(att_mat):
     v = joint_attentions[-1]
     grid_size = int(np.sqrt(aug_att_mat.size(-1)))
     mask = v[0, 1:].reshape(grid_size*2, grid_size//2).detach().numpy()
-    # mask = cv2.resize(mask / mask.max(), im.size)[..., np.newaxis]
-    # result = (mask * im).astype("uint8")
-
+    
     return mask
 
 def save_debug(data, pred_mask, vis_data):
