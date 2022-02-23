b706aaa6d9e857c4427d748dd6f73717055252ef
xiaozai <ysongonly@gmail.com>
2022-02-23 13:23:34 +0200

    vis attn weights

--- ltr/actors/depth_segm_actor.py
+++ ltr/actors/depth_segm_actor.py
@@ -29,8 +29,8 @@ def save_debug(data, pred_mask, vis_data):
         p_rgb, p_d = attn_weights3, attn_weights2
         n_patches = p_rgbd.shape[1]
         new_feat_sz = math.sqrt(n_patches)
-        p_rgb = p_rgb.view(:, new_feat_sz, new_feat_sz, :)
-        p_d = p_d.view(:, new_feat_sz, new_feat_sz, :)
+        p_rgb = p_rgb.view(p_rgb.shape[0], new_feat_sz, new_feat_sz, -1)
+        p_d = p_d.view(p_d.shape[0], new_feat_sz, new_feat_sz, -1)
         # vis_cosine_similarity = False
         print(p_rgb.shape)
     elif len(vis_data) == 5:
