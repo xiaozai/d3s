ee4600c191e05d79157e10a1c10f58f73f1950b7
xiaozai <ysongonly@gmail.com>
2022-03-04 12:13:13 +0200

    test atom fetch

--- ltr/train_settings/depth_segm/depth_segm_attention13.py
+++ ltr/train_settings/depth_segm/depth_segm_attention13.py
@@ -107,6 +107,7 @@ def run(settings):
     net = segm_models.depth_segm_attention13_resnet50(backbone_pretrained=True, topk_pos=settings.segm_topk_pos,
                                                     topk_neg=settings.segm_topk_neg, mixer_channels=mixer_channels)
 
+    # song test atom fetch
     # Set objective
     objective = nn.BCEWithLogitsLoss()
 
