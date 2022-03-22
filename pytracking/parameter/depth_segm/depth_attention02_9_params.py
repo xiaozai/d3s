f975f7ccd869c23694eea2169ec5223ef7ce0076
xiaozai <ysongonly@gmail.com>
2022-03-22 15:26:06 +0200

    evaluate A02_9 30epoch

--- pytracking/parameter/depth_segm/depth_attention02_9_params.py
+++ pytracking/parameter/depth_segm/depth_attention02_9_params.py
@@ -111,7 +111,7 @@ def parameters():
     params.model = 'depth_segm_st'
     params.constructor_module = 'ltr.models.depth_segm.depth_segm'
     params.constructor_fun_name = 'depth_segm_attention02_9_resnet50'
-    params.segm_net_path = '/home/yan/Data2/d3s/checkpoints/ltr/depth_segm/depth_segm_attention02_9/DepthSegmNet_ep0040.pth.tar'
+    params.segm_net_path = '/home/yan/Data2/d3s/checkpoints/ltr/depth_segm/depth_segm_attention02_9/DepthSegmNet_ep0030.pth.tar'
     params.segm_use_dist = True
     params.segm_normalize_mean = [0.485, 0.456, 0.406]
     params.segm_normalize_std = [0.229, 0.224, 0.225]
