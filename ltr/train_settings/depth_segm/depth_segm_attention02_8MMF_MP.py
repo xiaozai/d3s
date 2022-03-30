b9c60f46968040610ccacb1b3ff0f71949e78ec0
xiaozai <ysongonly@gmail.com>
2022-03-30 21:55:55 +0300

    fix

--- ltr/train_settings/depth_segm/depth_segm_attention02_8MMF_MP.py
+++ ltr/train_settings/depth_segm/depth_segm_attention02_8MMF_MP.py
@@ -102,7 +102,7 @@ def run(settings):
 
     # Create network
     # resnet50 or resnet18
-    net = segm_models.depth_segm_attention02_8MMF_resnet50(backbone_pretrained=True, topk_pos=settings.segm_topk_pos,
+    net = segm_models.depth_segm_attention02_8MMF_MP_resnet50(backbone_pretrained=True, topk_pos=settings.segm_topk_pos,
                                                         topk_neg=settings.segm_topk_neg, mixer_channels=mixer_channels)
 
     # Set objective
