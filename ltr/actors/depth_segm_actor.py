80477d31c424423c1093d14d44789c9cbe5d35d9
xiaozai <ysongonly@gmail.com>
2022-04-05 16:54:35 +0300

    fix

--- ltr/actors/depth_segm_actor.py
+++ ltr/actors/depth_segm_actor.py
@@ -223,7 +223,7 @@ def save_debug_MP(data, pred_mask, vis_data, batch_element = 0):
         elif len(vis_data) == 4:
             attn_weights3, attn_weights2, attn_weights1, attn_weights0 = vis_data
 
-            if isinstance(attn_weights3, list) or isinstance(attn_weights3, tuple) or len(attn_weights3) == 5:
+            if isinstance(attn_weights3, list) or isinstance(attn_weights3, tuple) or len(attn_weights3.shape) == 5:
                 ''' transformer attn maps '''
                 attn_weights3 = process_attn_maps(attn_weights3, batch_element, layer=3)
                 attn_weights2 = process_attn_maps(attn_weights2, batch_element, layer=2)
