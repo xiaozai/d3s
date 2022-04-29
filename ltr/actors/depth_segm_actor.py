203a257e7830cfbdac4ff264c9feea43663ae54a
xiaozai <ysongonly@gmail.com>
2022-04-29 17:40:40 +0300

    fix

--- ltr/actors/depth_segm_actor.py
+++ ltr/actors/depth_segm_actor.py
@@ -278,13 +278,19 @@ def save_debug_MP(data, pred_mask, vis_data, batch_element = 0):
     train_depth = data['train_depths'][:, batch_element, :, :].permute(1, 2, 0) # .numpy().squeeze().astype(np.float32)
     test_depth = data['test_depths'][:, batch_element, :, :].permute(1, 2, 0) #.numpy().squeeze().astype(np.float32)
 
-    if train_depth.shape[-1] == 3:
-        train_depth = 255 * (train_depth * std + mu)
-        test_depth = 255 * (test_depth * std + mu)
+    # if train_depth.shape[-1] == 3:
+    #     train_depth = 255 * (train_depth * std + mu)
+    #     test_depth = 255 * (test_depth * std + mu)
 
     train_depth = train_depth.numpy().squeeze() #.astype(np.uint8)
     test_depth = test_depth.numpy().squeeze() # .astype(np.uint8)
 
+    if train_depth.shape[-1] == 3:
+        train_depth = 255 * (train_depth * std + mu)
+        test_depth = 255 * (test_depth * std + mu)
+
+        train_depth = train_depth.astype(np.uint8)
+        test_depth = test_depth.astype(np.uint8)
 
     train_mask = data['train_masks'][0, batch_element, :, :].numpy().astype(np.float32)
     test_mask = data['test_masks'][0, batch_element, :, :].numpy().astype(np.float32)
