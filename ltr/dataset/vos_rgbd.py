dc4d4e611ec51c9da73a74e01d9373092a28b5e5
xiaozai <ysongonly@gmail.com>
2022-04-29 17:05:22 +0300

    fix

--- ltr/dataset/vos_rgbd.py
+++ ltr/dataset/vos_rgbd.py
@@ -111,8 +111,9 @@ class Vos_rgbd(BaseDataset):
             depth = cv2.normalize(depth, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
             if self.use_colormap:
                 print('convert depth to colormap')
-                depth = np.asarray(depth * 255, dtype=np.uint8)
-                depth = cv2.applyColorMap(depth, cv2.COLORMAP_JET)
+                depth = np.asarray(depth*255, dtype=np.uint8)
+                depth = cv.applyColorMap(depth, cv.COLORMAP_JET)
+                print('depth : ', depth.shape)
                 print('Done')
                 return np.asarray(depth, axis=-1)
             else:
