e96e817bb56d4b967f07c563b90bc289121f728c
xiaozai <ysongonly@gmail.com>
2022-05-03 21:18:27 +0300

    fix

--- pytracking/tracker/depth_segm/optim.py
+++ pytracking/tracker/depth_segm/optim.py
@@ -36,7 +36,7 @@ class FactorizedConvProblem(optimization.L2Problem):
         else:
             compressed_samples = self.training_samples
 
-        print('Song : in FactorizedConvProblem: compressed_samples:', compressed_samples.shape)
+        print('Song : in FactorizedConvProblem: compressed_samples:', compressed_samples[0].shape, self.training_samples_d.shape)
         # Do second convolution,
         residuals = operation.conv2d(compressed_samples, filter, mode='same', depth=self.training_samples_d).apply(self.response_activation)
 
@@ -96,7 +96,7 @@ class ConvProblem(optimization.L2Problem):
         Song, add depthconv, does not work, since it requires pytorch 0.4.0
         """
         # Do convolution and compute residuals
-        print('self.training_samples in ConvProblem: ', self.training_samples.shape)
+        print('self.training_samples in ConvProblem: ', self.training_samples.shape, self.training_samples_d.shape)
         residuals = operation.conv2d(self.training_samples, x, mode='same', depth=self.training_samples_d).apply(self.response_activation)
         residuals = residuals - self.y
 
