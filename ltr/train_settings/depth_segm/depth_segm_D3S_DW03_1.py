a42f3cfa76153c203b8ff9e1880faf6cf1b64442
xiaozai <ysongonly@gmail.com>
2022-05-19 12:29:48 +0300

    fix

--- ltr/train_settings/depth_segm/depth_segm_D3S_DW03_1.py
+++ ltr/train_settings/depth_segm/depth_segm_D3S_DW03_1.py
@@ -39,7 +39,7 @@ def run(settings):
     if not os.path.isdir(settings.env.workspace_dir):
         os.mkdir(settings.env.workspace_dir)
 
-    settings.env.images_dir = os.path.join(settings.env.workspace_dir, 'images_D3S_DW03')
+    settings.env.images_dir = os.path.join(settings.env.workspace_dir, 'images_D3S_DW03_1')
     if not os.path.isdir(settings.env.images_dir):
         os.mkdir(settings.env.images_dir)
 
